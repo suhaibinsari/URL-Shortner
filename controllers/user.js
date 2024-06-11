62377c0d1eb14643135978f69b01e79908182eb2
suhaibinsari <mstar390@gmail.com>
2024-06-11 22:00:14 +0500

    jsonwebtoken/stateless

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const USER = require("../models/user")
 const { v4: uuidv4 } = require('uuid')
-const { setUser, getUser } = require("../service/auth")
+const { setUser } = require("../service/auth")
 
 
 async function handleUserSignup(req, res) {
@@ -19,10 +19,16 @@ async function handleUserLogin(req, res) {
     if (!user) return res.render("logIn", {
         error: "Invalid User Name or Password"
     })
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie("uid", sessionId)
-    return res.redirect('/')
+
+    // const sessionId = uuidv4(); // for stateful
+    // setUser(sessionId, user);
+    // res.cookie("uid", sessionId)
+    // return res.redirect('/')
+
+
+    const token = setUser(user); // for stateless
+    res.cookie("uid", token)
+    return res.redirect("/")
 }
 
 
--- service/auth.js
+++ service/auth.js
@@ -1,12 +1,40 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map(); // used to create stateful
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user)
+// function setUser(id, user) {
+//     sessionIdToUserMap.set(id, user)
+// }
+
+// function getUser(id) {
+//     return sessionIdToUserMap.get(id)
+// }
+
+// module.exports = {
+//     setUser,
+//     getUser
+// }
+
+
+
+const jwt = require("jsonwebtoken") // used to create stateless
+const secret = "MOON123"
+
+function setUser(user) {
+    return jwt.sign({
+        _id: user._id,
+        email: user.email
+    }, secret
+    )
 }
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id)
+function getUser(token) {
+    if (!token) return null;
+    try {
+        return jwt.verify(token, secret)
+    } catch (error) {
+        return null;
+    }
 }
+
 
 module.exports = {
     setUser,
